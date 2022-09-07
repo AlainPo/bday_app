09c6cc7e65933f4ffdec839ba9a6a32ee72c25da
AlainPo <109284542+AlainPo@users.noreply.github.com>
2022-09-07 10:37:32 +0700

    Add files via upload

---
+++ BDayFile.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace bdays_app2
+{
+    internal class BDayFile
+    {
+        public string path = "";    //путь к файлу
+
+        public BDayFile(string path = "")
+        {
+            this.path = path;
+        }
+
+        public bool PrintFile()
+            {
+                StreamReader sr;
+
+                try
+                {
+                    sr = new StreamReader(path);
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("Error: {0}", e.Message);
+                    return false;
+                }
+
+                string s; // дополнительная переменная
+
+                Console.WriteLine("The content of file {0}:", path);
+
+                s = sr.ReadLine(); // прочитать первую строку из файла
+                while (s != null) // конець файла - значение null
+                {
+                    s= s.Replace(" ", "\t\t");
+    
+                    Console.WriteLine(s);
+
+                    s = sr.ReadLine();
+                }
+
+                sr.Close();
+
+                return true;
+        }
+
+        public bool PrintTodayNames(string filename)
+        {
+            StreamReader sr;
+
+            try
+            {
+                sr = new StreamReader(filename);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return false;
+            }
+
+            string s; // дополнительная переменная
+
+            Console.WriteLine("\n\nToday bdays:");
+
+            s = sr.ReadLine(); // прочитать первую строку из файла
+            while (s != null) // конець файла - значение null
+            {
+                string[] info = s.Split(" ");
+                DateTime readDate = new DateTime(DateTime.Today.Year, Convert.ToInt32(Convert.ToDateTime(info[1]).Month), Convert.ToInt32(Convert.ToDateTime(info[1]).Day));
+                if (readDate==DateTime.Today)
+                    Console.WriteLine($"{info[0]}\t\t{info[1]}");
+                s = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            return true;
+        }
+
+        public bool PrintSoonNames(string filename)
+        {
+            Console.WriteLine("\n\nUpcoming bdays:");
+            StreamReader sr;
+
+            try
+            {
+                sr = new StreamReader(filename);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return false;
+            }
+
+            string s; // дополнительная переменная
+
+            s = sr.ReadLine(); // прочитать первую строку из файла
+            while (s != null) // конець файла - значение null
+            {
+                string[] info = s.Split(" ");
+                DateTime readDate = new DateTime(DateTime.Today.Year, Convert.ToInt32(Convert.ToDateTime(info[1]).Month), Convert.ToInt32(Convert.ToDateTime(info[1]).Day));
+                if (readDate.Date > DateTime.Today && readDate.Date < DateTime.Today.AddDays(7))
+                    Console.WriteLine($"{info[0]}\t\t{info[1]}");
+                s = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            return true;
+        }
+
+
+        public bool ChangeRec(int position, string str)
+        {
+
+            if (!File.Exists(path)) return false;
+
+            string[] arrayS = File.ReadAllLines(path);
+
+            if ((position < 0) || (position >= arrayS.Length))
+                return false;
+
+            arrayS[position] = str;
+
+            File.WriteAllLines(path, arrayS);
+
+            return true;
+        }
+
+        public bool DeleteRec(int index)
+        {
+            StreamReader sr;
+
+            try
+            {
+                sr = File.OpenText(path);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            List<string> lst = new List<string>(0); // создать пустой список
+            string s;
+            while ((s = sr.ReadLine()) != null)
+                lst.Add(s); // добавить строку в список
+
+            sr.Close();
+
+            if ((index < 0) || (index >= lst.Count))
+                return false;
+
+            lst.RemoveAt(index);
+
+            StreamWriter sw = new StreamWriter(path); // открыть файл для записи
+
+            for (int i = 0; i < lst.Count; i++)
+                sw.WriteLine(lst[i]);
+
+            sw.Close();
+
+            return true;
+        }
+
+        public bool AddRec(string str)
+        {
+            StreamReader sr;
+            try
+            {
+                sr = new StreamReader(path);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            List<string> lst = new List<string>(0); // создать пустой список
+            string s;
+            while ((s = sr.ReadLine()) != null) { 
+                lst.Add(s); // добавить строку в список
+
+             }
+            sr.Close();
+
+            lst.Add(str);
+
+            StreamWriter sw = new StreamWriter(path);
+
+            foreach (string ts in lst)
+                sw.WriteLine(ts);
+
+            sw.Close();
+
+            return true;
+        }
+
+
+
+        public int Exist(string str, DateTime date) //1-both fields, 0-only name
+        {
+            int flag = 0;
+            StreamReader sr;
+            try
+            {
+                sr = new StreamReader(path);
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+
+            string s;
+            int position = 0;
+
+            while ((s = sr.ReadLine()) != null)
+            {
+                string[] rec = s.Split(" ");
+                string name = rec[0];
+
+                if (name == str)
+                {
+                    sr.Close();
+                    return position;
+                }
+                position++;
+            }
+
+            position = -1;
+            sr.Close();
+            return position;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.IO;
+using System.IO.Enumeration;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Xml.Serialization;
+
+namespace bdays_app2
+{
+    class Program
+    {
+
+        static int PrintMenu(BDayFile file)
+        {
+            int choice=0;
+
+            try
+            {
+                Console.WriteLine("\nChoose action:\n\n1-Add record\n2-Edit record\n3-Delete record\n4-Full table\n5-Quit\n");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+                ChoosePath(choice, file);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return choice ;
+        }
+
+        static void ChoosePath(int choice, BDayFile file)
+        {
+            try {
+                switch (choice)
+                {
+                    case 1:
+                        AddRecord(file);
+                        break;
+                    case 2:
+                        ChangeRecord(file);
+                        break;
+                    case 3:
+                        DeleteRecord(file);
+                        break;
+                    case 4:
+                        file.PrintFile();
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void AddRecord(BDayFile file)
+        {
+            try
+            {
+                Console.WriteLine("Type name:");
+                string name = Console.ReadLine().ToString();
+                DateTime bday=DateTime.Today;
+
+                if (file.Exist(name, bday)!=-1)
+                    Console.WriteLine("The record with that name already exists!");
+                else
+                {
+                    Console.WriteLine("Type birth date (format dd/mm/yyyy or dd.mm.yyyy):");
+                    bday = Convert.ToDateTime(Console.ReadLine()).Date;
+                    string rec = name + " " + bday.ToShortDateString();
+                    file.AddRec(rec);
+                    Console.WriteLine("The record has been added successfully!");
+                }
+
+                file.PrintTodayNames(file.path);
+                file.PrintSoonNames(file.path);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ChangeRecord(BDayFile file)
+        {
+            try
+            {
+                Console.WriteLine("Type existing name:");
+                string name = Console.ReadLine().ToString();
+                DateTime bday=DateTime.Today;
+
+
+                int pos = file.Exist(name, bday);
+                if (pos==-1)
+                    Console.WriteLine("The record doesn't exists!");
+                else
+                {
+                    Console.WriteLine("Type new birth date (format dd/mm/yyyy or dd.mm.yyyy):");
+                    bday = Convert.ToDateTime(Console.ReadLine()).Date;
+                    string rec = name + " " + bday.ToShortDateString();
+                    file.ChangeRec(pos, rec);
+                    Console.WriteLine("The record has been changed successfully!");
+                }
+
+                file.PrintTodayNames(file.path);
+                file.PrintSoonNames(file.path);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void DeleteRecord(BDayFile file)
+        {
+            try
+            {
+                Console.WriteLine("Type existing name:");
+                string name = Console.ReadLine().ToString();
+                //Console.WriteLine("Type its birth date (format dd/mm/yyyy):");
+                DateTime bday;
+                //bday = Convert.ToDateTime(Console.ReadLine()).Date;
+                bday = DateTime.Today;
+                int pos = file.Exist(name, bday);
+                if (pos == -1)
+                    Console.WriteLine("The record doesn't exists!");
+                else
+                {
+                    file.DeleteRec(pos);
+                    Console.WriteLine("The record has been deleted successfully!");
+                }
+
+                file.PrintTodayNames(file.path);
+                file.PrintSoonNames(file.path);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            string path = "..\\bdays.txt";
+            BDayFile file = new BDayFile(path);
+
+            file.PrintTodayNames(path);
+            file.PrintSoonNames(path);
+            int choice = 0;
+            while (choice!=5)
+            {
+                choice=PrintMenu(file);
+            }
+            Console.WriteLine("Come back soon!");
+            return 0;
+
+        }
+    }
+}
